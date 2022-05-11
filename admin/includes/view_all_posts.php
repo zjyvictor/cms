f5525ab6eb32cb4b341c6866fc0b95b9b504dbe1
Victor <zhangj7841@uwec.edu>
2022-05-11 18:26:33 +0800

    114. Deleting Posts in Admin

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -39,7 +39,7 @@
                 echo "<td>{$post_tags}</td>";
                 echo "<td>{$post_comment_count}</td>";
                 echo "<td>{$post_date}</td>";
-
+                echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
                 echo "</tr>";
             }
             ?>
@@ -57,3 +57,16 @@
                                 </tr> -->
      </tbody>
  </table>
+
+ <?php
+    if(isset($_GET['delete'])){
+       $the_post_id = $_GET['delete'];
+       $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+       $delete_query = mysqli_query($conn,$query);
+       if(!$delete_query){
+        die('Delete post query FAILED! ' . mysqli_error($conn));
+       }else{
+           header("Location: ./posts.php");
+       }
+    }
+ ?>
