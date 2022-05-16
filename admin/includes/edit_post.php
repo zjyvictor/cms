4c71b7a0c3e44452787010a89d89ae765d6610e3
Victor <zhangj7841@uwec.edu>
2022-05-16 16:49:47 +0800

    116. Displaying the Edit Data in Post Edit Page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,19 +1,42 @@
+<?php
+
+    if(isset($_GET['p_id'])){
+        $the_post_id = $_GET['p_id'];
+
+
+    }
+    $query = "SELECT * FROM posts";
+    $select_post_by_id = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_assoc($select_post_by_id)) {
+        $post_id = $row['post_id'];
+        $post_category_id = $row['post_category_id'];
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_image = $row['post_image'];
+        $post_tags = $row['post_tags'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_date = $row['post_date'];
+        $post_status = $row['post_status'];
+        $post_content = $row['post_content'];
+    }
+?>
+
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="post_title">Post Title</label>
-        <input type="text" class="form-control" name="post_title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
     <div class="form-group">
         <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <input value="<?php echo $post_category_id; ?>" type="text" class="form-control" name="post_category_id">
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>
-        <input type="text" class="form-control" name="post_author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
     </div>
     <div class="form-group">
         <label for="title">Post Image</label>
@@ -21,11 +44,11 @@
     </div>
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input value="<?php echo $post_tags; ?>"  type="text" class="form-control" name="post_tags">
     </div>
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
     </div>
     <div class="form-group">
        
