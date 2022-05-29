46d2b6a63f1c674b866c1836b0c0f2474ff66480
Victor <zhangj7841@uwec.edu>
2022-05-29 17:14:49 +0800

    120. Relating Categories to Posts and Displaying it

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -33,7 +33,14 @@
                 echo "<td>{$post_id}</td>";
                 echo "<td>{$post_author}</td>";
                 echo "<td>{$post_title}</td>";
-                echo "<td>{$post_category_id}</td>";
+
+                $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
+                $select_category_by_id = mysqli_query($conn, $query);
+                while ($row = mysqli_fetch_assoc($select_category_by_id)) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                    echo "<td>{$cat_title}</td>";
+                }
                 echo "<td>{$post_status}</td>";
                 echo "<td><img src='../images/$post_image' width=100 alt='image'></td>";
                 echo "<td>{$post_tags}</td>";
