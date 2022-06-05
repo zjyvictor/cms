16e4202e681a06f040667578bd149247e3c86706
Victor <zhangj7841@uwec.edu>
2022-06-05 15:57:26 +0800

    123. Creating the category page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -29,8 +29,9 @@
                 $select_categories_sidebar = mysqli_query($conn, $query);
 
                 while($row = mysqli_fetch_assoc($select_categories_sidebar)){
+                    $cat_id = $row['cat_id'];
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                 }
             ?>
                
